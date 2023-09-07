6823a077e8415b7e05c3de2b639a0a98cbef0c80
Squidly271 <unraidsquid@gmail.com>
2023-09-07 14:56:45 -0400

    Update copy_to_git.sh

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -4,5 +4,5 @@ mkdir -p "/tmp/GitHub/gui.search/source/gui.search/usr/local/emhttp/plugins/gui.
 
 cp /usr/local/emhttp/plugins/gui.search/* /tmp/GitHub/gui.search/source/gui.search/usr/local/emhttp/plugins/gui.search -R -v -p
 cd /tmp/GitHub/gui.search/source/gui.search/usr/local/emhttp/plugins/gui.search
-
+find . -maxdepth 9999 -noleaf -type f -name "._*" -exec rm -v "{}" \;
 
